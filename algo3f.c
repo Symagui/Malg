37dda764d002e4a98fae1186aef504ba4e85b178
Symagui <guillaume.ruchot@telecomnancy.net>
2017-04-09 15:20:25 +0200

    j'ai essayé

--- algo3f.c
+++ algo3f.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-/*@ axiomatic Solve {
-  logic integer solve(int n);
+#include <limits.h>
+int inmax = INT_MAX;
+/*@
+axiomatic Solve {
+  logic integer solve(integer n);
 
-  axiom solve_0: int n = 0; count(n) == 1;
+  axiom solve_0: solve(0) ==  1;
 
-  axiom solve_1: int n = 1; count(n) == 1;
+  axiom solve_1: solve(1) ==  1;
 
-  axiom solve_n: \forall int n; count(n) == 2*count(n-2)+2*count(n-1);
+  axiom solve_n: \forall integer n;  n>1 ==> solve(n) == 2*solve(n-2)+2*solve(n-1);
 }*/
 
 
-/* requires 0 <= x && \valid(x)
-    ensures \result == solve(x);
-
+/*@ 
+requires 0 <= x <= 24;
+ensures \result > 0;
+ensures \result == solve(x);
 */
 int p1(int x){
   int a,b,c,i,r,fin;
@@ -21,28 +25,35 @@ int p1(int x){
   i=2;
   if (x==0) {
     r=1;
+  } else if (x==1) {
+    r=1;
   } else {
-    if (x==1) {
-      r=1;
-    } else {
-      while (i-1 < x){
-        i=i+1;
-	      c=a;
-	      a=b;
-	      b=2*c+2*b;
-	     }
-     }
-    r=b;
+		//@ assert x <= 24;
+		//@ loop invariant a>0 && b>0 && b == solve(i-1);
+    while (i-1 < x){
+			//@ assert i <= x;
+      i=i+1;
+	    c=a;
+	    a=b;
+	    b=2*c+2*b;
+			printf("%d\n", b);
+	  }
+  	r=b;
   }
   return(r);
 }
 
 
-
+/*@ 
+		ensures \result == 0;
+*/
 int main(){
-  int v;
-  printf("Entrez la valeur  pour  v\n");
-  scanf("%d", &v);
+  int v = -1;
+	while (v < 0 || v >= 25){
+  	printf("Entrez la valeur  pour  v\n");
+  	scanf("%d", &v);
+	}
+	//@ assert v >= 0 && v< 25;
   printf(" voici la réponse de votre solution p2(%d)=%d\n",v,p1(v));
   return 0;
 }
